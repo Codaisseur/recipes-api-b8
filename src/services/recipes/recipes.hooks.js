2a6b912fa8a06c0a889a8b94db77273ab59acef4
Wouter de Vos <wouter@codaisseur.com>
2017-06-15 11:02:20 +0200

    Seeds and hooks for recipes and users to populate the recipe authors

--- src/services/recipes/recipes.hooks.js
+++ src/services/recipes/recipes.hooks.js
@@ -1,18 +1,62 @@
+// src/services/recipes/recipes.hooks.js
 
+const { authenticate } = require('feathers-authentication').hooks;
+const { restrictToOwner, associateCurrentUser, restrictToAuthenticated } = require('feathers-authentication-hooks');
+const { populate } = require('feathers-hooks-common');
+
+// Configure where we will get the author data from (the users service),
+// how to fetch it (by authorId), and where to put it (author).
+const authorSchema = {
+  include: {
+    service: 'users',
+    nameAs: 'author',
+    parentField: 'authorId',
+    childField: '_id',
+  }
+};
+
+const likerSchema = {
+  include: {
+    service: 'users',
+    nameAs: 'likers',
+    parentField: 'likedBy',
+    childField: '_id'
+  }
+};
+
+const restrict = [
+  authenticate('jwt'),
+  restrictToOwner({
+    ownerField: 'authorId'
+  })
+];
 
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    create: [
+      authenticate('jwt'),
+      restrictToAuthenticated(),
+      associateCurrentUser({ as: 'authorId' }),
+    ],
+    update: [
+      ...restrict
+    ],
+    patch: [
+      ...restrict
+    ],
+    remove: [
+      ...restrict
+    ]
   },
 
   after: {
-    all: [],
+    all: [
+      populate({ schema: authorSchema }),
+      // populate({ schema: likerSchema }),
+    ],
     find: [],
     get: [],
     create: [],
