efcd4987ecbc126a17669064a06ec9ed6cc3f28a
Wouter de Vos <wouter@codaisseur.com>
2017-06-19 15:07:29 +0200

    Remove debugging

--- src/services/recipes/recipes.filters.js
+++ src/services/recipes/recipes.filters.js
@@ -2,8 +2,5 @@
 console.warn('You are using the default filter for the recipes service. For more information about event filters see https://docs.feathersjs.com/api/events.html#event-filtering'); // eslint-disable-line no-console
 
 module.exports = function (data, connection, hook) { // eslint-disable-line no-unused-vars
-  console.log(hook.params.user && hook.params.user._id.toString())
-  // console.log(data)
-  debugger
   return data;
 };
